30c4395c76a8e51a1b0f750574024356fc8774c2
imvenx <imvenx@proton.me>
2023-12-02 02:10:15 +0100

    first approach

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,41 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header style="  background: linear-gradient(120deg, rgb(0, 67, 67), rgb(67, 0, 67));    color: cyan;">
+      <q-toolbar>
+        <q-btn flat dense round outline icon="diamond" aria-label="Menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title>
+          Arcanepad
+        </q-toolbar-title>
+
+        <!-- <div>Quasar v{{ $q.version }}</div> -->
+        <q-btn flat dense round outline icon="" aria-label="Menu" @click="toggleLeftDrawer" />
+      </q-toolbar>
+    </q-header>
+
+    <!-- <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
+      <q-list>
+        <q-item-label header>
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink v-for="link in essentialLinks" :key="link.title" v-bind="link" />
+      </q-list>
+    </q-drawer> -->
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import EssentialLink, { EssentialLinkProps } from 'components/EssentialLink.vue';
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer() {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-page class="row items-center justify-evenly" style="background-color: rgba(0, 0, 0, 0.7);">
+    <div style="display: grid; gap: 30px; width: 75%; max-width: 350px;" class="text-center">
+      <br style="margin: 2px;">
+      <div class="gradient-container">
+        <q-img src="/images/logo.png" alt="Logo" />
+      </div>
+
+      <div class="text-h5">
+        A new way of playing
+      </div>
+      <hr style="width: 100%;" />
+
+      <div class="text-h4">Download</div>
+      <div style="display: grid; grid-template-columns: 40% 50%; gap: 10%;">
+        <div class="card">
+          <div class="text-h6">Mobile</div>
+          <q-icon style="margin: auto;" name="smartphone" size="xl" />
+          <q-btn>Android</q-btn>
+        </div>
+
+        <div>
+          <q-icon name="laptop" size="xl" />
+        </div>
+      </div>
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+      <hr />
+    </div>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid gray;
+  border-radius: 5px;
+  padding: 10px;
+  display: grid;
+  text-align: center;
+  justify-content: center;
+  gap: 5px;
+}
+
+.gradient-container {
+  background: linear-gradient(120deg, cyan, fuchsia);
+  display: inline-block;
+  padding: 1em;
+  border-radius: 1000px;
+  box-shadow: 0px 0px 20px cyan;
+}
+
+.gradient-container img {
+  display: block;
+  border-radius: 1000px;
+  width: 100%;
+  height: auto;
+}
+</style>
