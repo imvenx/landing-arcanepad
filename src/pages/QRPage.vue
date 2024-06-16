a9a7d78879d3e3a8c129008ef778e4b30b09b971
imvenx <imvenx@proton.me>
2024-06-16 18:53:02 +0200

    add cool arcanepad text to qr mobile

--- src/pages/QRPage.vue
+++ src/pages/QRPage.vue
@@ -1,10 +1,15 @@
 <template>
+  <div style="text-align: center; padding: 4em;" class="hideOnLandscape">
+    <ArcanepadText />
+  </div>
   <div style="max-width: 500px">
     <q-img src="/images/QRWeb.png"></q-img>
   </div>
+
 </template>
 
 <script setup lang="ts">
+import ArcanepadText from 'src/components/ArcanepadText.vue';
 </script>
 
 <style scoped></style>
